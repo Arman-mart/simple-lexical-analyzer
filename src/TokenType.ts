d3cc38aa8d8b61b3732ee2907e757f6869093c10
yourusername <your@email.com>
2023-01-14 16:13:31 +0400

    feat: add token and tokenType

---
+++ src/TokenType.ts
@@ -0,0 +1,22 @@
+export default class TokenType {
+    name: string;
+    regex: string;
+
+    constructor(name: string, regex: string) {
+        this.name = name;
+        this.regex = regex;
+    }
+}
+
+export const TOKEN_TYPE_LIST = {
+    'NUMBER': new TokenType('NUMBER', '[0-9]*'),
+    'VARIABLE': new TokenType('VARIABLE', '[A-Za-z]*'),
+    'SEMICOLON': new TokenType('SEMICOLON', ';'),
+    'SPACE': new TokenType('SPACE', '[ \\n\\t\\r]'),
+    'ASSIGN': new TokenType('ASSIGN', 'ASSIGN'),
+    'LOG': new TokenType('LOG', 'CONSOLE'),
+    'PLUS': new TokenType('PLUS', 'PLUS'),
+    'MINUS': new TokenType('MINUS', 'MINUS'),
+    'LPAR': new TokenType('LPAR', '\\('),
+    'RPAR': new TokenType('RPAR', '\\)'),
+}
