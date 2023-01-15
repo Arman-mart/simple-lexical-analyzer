5c711421cf989c16d588799679301b16a9292a4c
yourusername <your@email.com>
2023-01-15 19:06:01 +0400

    feat: Add mandatory token matching to Parser class for improved error handling

--- src/Parser.ts
+++ src/Parser.ts
@@ -13,11 +13,21 @@ export default class Parser {
     match(...expected: TokenType[]): Token | null {
         if (this.pos < this.tokens.length) {
             const currentToken = this.tokens[this.pos];
-            if(expected.find( type => type.name === currentToken.type.name)){
+            if (expected.find(type => type.name === currentToken.type.name)) {
                 this.pos++;
                 return currentToken;
             }
         }
         return null
     }
+
+    require(...expected: TokenType[]): Token {
+        const token = this.match(...expected);
+        if (!token) {
+            throw new Error(`${expected[0].name} is expected at position ${this.pos}`)
+        }
+        return token;
+    }
+
+    
 }
